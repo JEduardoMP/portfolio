e1c1d92ff3110361237b6c40bbda0f697eab1701
Jose Eduardo <eduardomarque2009@gmail.com>
2021-08-17 21:49:10 -0500

    fixing the nav with js

--- js/scroll.js
+++ js/scroll.js
@@ -1,17 +1,21 @@
+// Sections
+const about = document.getElementById('about-me');
+const services = document.getElementById('services-section');
+const contact = document.getElementById('contact-me');
+const main = document.getElementById('main-menu');
+
+// Top sections
+let aboutOffset = about.offsetHeight - 60;
+let servicesOffset = services.offsetHeight;
+let contactOffset = contact.offsetHeight;
+
+// Nav buttons
+const aboutNav = document.getElementsByClassName('navigation');
+
 function scrollMenu() {
-    for (let i = 0; i < main.length; i++) {
-        console.log(i)
+    for (let i = 0; i < aboutNav.length; i++) {
+        const element = aboutNav[i];
+        console.log(aboutNav[0])
     }
-    // if (main.onclick) {
-    //     window.scrollTo(0, about.offsetHeight - 60);
-    // } else if (document.onclick = main) {
-    //     window.scrollTo(0, services.offsetHeight - 60);
-    // } else {
-    //     window.scrollTo(0, contact.offsetHeight - 60);
-    // }
 }
 document.onclick = scrollMenu
-var about = document.getElementById('about-me');
-var services = document.getElementById('services-section');
-var contact = document.getElementById('contact-me')
-var main = document.getElementById('main-menu')
